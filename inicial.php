8f22f1cf54313e401cff6f28ffaef5aa2ad98f2a
devRafaelFN <rafaelpontesfn4@gmail.com>
2024-11-23 01:04:09 -0300

    Inicial atualizado

---
+++ inicial.php
@@ -0,0 +1,45 @@
+<html lang="pt-BR"> 
+    <head> <meta charset="UTF-8">
+     <title>Barber Manager</title>
+      <link rel="stylesheet" type="text/css" href="estilo/style.css">
+     </head>
+
+<body>
+    <div class="conteudo-principal">
+        <h2>Bem-vindo à Nossa Barbearia!</h2>
+        <p>Oferecemos os melhores serviços de corte de cabelo e barba com qualidade e profissionalismo.</p>
+
+        <div class="imagem-container">
+            <img src="imagem.barber.png" alt="Imagem da Barbearia" width="420" height="100">
+        </div>
+
+
+
+        <!-- Serviços -->
+        <section class="servicos">
+            <h3>Serviços</h3>
+            <ul>
+                <li>Corte de cabelo masculino</li>
+                <li>Corte de barba</li>
+                <li>Design de sobrancelhas</li>
+            </ul>
+        </section>
+
+        <!-- Localização -->
+        <section class="localizacao">
+            <h3>Nosso Endereço</h3>
+            <p>Estamos localizados no centro da cidade</p>
+        </section>
+
+        <!-- Contatos -->
+        <section class="contatos">
+            <h3>Contato</h3>
+            <p>Telefone para contato:</p>
+            <p><strong>Telefone:</strong> (81) 98168-6973</p>
+            <a href="tel:+5581981686973" class="cta-btn">Ligue Agora</a>
+        </section>
+    </div>
+
+
+</body>
+</html>
