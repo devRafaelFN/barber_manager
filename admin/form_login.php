1764d664c2f6874165c974d7c251cd8830852ac0
devRafaelFN <rafaelpontesfn4@gmail.com>
2024-11-23 01:06:28 -0300

    Adição do botão Voltar

--- admin/form_login.php
+++ admin/form_login.php
@@ -1,3 +1,15 @@
+<head>
+<meta charset="UTF-8">
+     <title>Login</title>
+      <link rel="stylesheet" type="text/css" href="estilo/style.css">
+</head>
+
+<section class="login">
+
+<h3>Login</h3>
+
+<p>Acesse sua conta para aproveitar nossos benefícios exclusivos!</p>
+
 <form action="login.php" method="post">
 
     Usuário: <input type="text" name="usuario"><br>
@@ -7,3 +19,9 @@
     <input type="submit" value="Entrar">
 
 </form>
+
+<p>Não tem uma conta? <a href="../form_cadastro.php" class="cta-btn">Cadastre-se aqui!</a></p>
+
+</section>
+
+<a href="../index.php">Voltar</a>
