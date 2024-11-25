41669717aa24db553f1834da83bb28419836ff79
DevMatheusR <matheus_henrique_9@hotmail.com>
2024-11-25 19:18:14 -0300

    Add files via upload

---
+++ cliente/formAgenda.php
@@ -0,0 +1,12 @@
+<h3>MARQUE AQUI SEU HORÁRIO</h3>
+
+<form action="?pg=agendamento" method="post">
+    Nome: <input type="text" name="nome" required><br> <!-- Colocar o nome do cliente -->
+    Horário: <input type="text" name="horario" required><br> <!-- Colocar o horário-->
+    Data: <input type="number" name="data" required><br> <!-- Colocar a data desejada -->
+    Observação: <input type="text" name="observacao"><br> <!-- Falar alguma especificação ou orientação -->
+
+    <input type="submit" value="MARCAR"><br> <!-- Botão para agendar! -->
+
+
+</form>
