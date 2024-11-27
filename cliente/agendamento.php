d5a29385b81bec574150b36d97b0ef91a3a95110
devRafaelFN <rafaelpontesfn4@gmail.com>
2024-11-27 20:04:09 -0300

    Update agendamento.php

--- cliente/agendamento.php
+++ cliente/agendamento.php
@@ -5,14 +5,14 @@ include_once('config.inc.php'); // 1° e 2° Etapa estabelecer uma conexão com 
 // Parte que irá receber as informações para o form;
 
 $nome = $_REQUEST['nome'];
-$horario = $_REQUEST['horario'];
 $data = $_REQUEST['data'];
+$hora = $_REQUEST['hora'];
 $observacao = $_REQUEST['observacao'];
 
 // 3° Etapa executar a consulta feita pelo SQL;
 
-$sql = "INSERT INTO agenda (nome, horario, data, observacao)
-        VALUES ('$nome', '$horario', '$data', '$observacao')";
+$sql = "INSERT INTO agenda (nome, data, hora, observacao)
+        VALUES ('$nome','$data','$hora', '$observacao')";
 
 $query = mysqli_query($conexao, $sql); // Executando o SQL;
 
