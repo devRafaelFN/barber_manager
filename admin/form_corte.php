fef7d51926cf26d32af5107d9ed4d0eb4167afc8
DevMatheusR <matheus_henrique_9@hotmail.com>
2024-11-27 19:57:27 -0300

    Add files via upload

---
+++ admin/form_corte.php
@@ -0,0 +1,11 @@
+<h3>CORTE NOVO NA ÁREA</h3>
+
+<form action="?pg=cadastra_corte" method="post">
+    Nome do corte: <input type="text" name="corte" required><br> <!-- Colocar o nome do corte -->
+    Tempo do corte: <input type="text" name="tempo_execucao" required><br> <!-- Colocar o tempo de execução-->
+    Valor: <input type="number" name="valor" required><br> <!-- Colocar o valor do corte -->
+
+    <input type="submit" value="CADASTRAR"><br> <!-- Botão para cadastrar! -->
+
+
+</form>
