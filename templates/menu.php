e6befbaff127a5ba3b012cd8e066252fc040eed8
devRafaelFN <rafaelpontesfn4@gmail.com>
2024-11-27 20:15:36 -0300

    Add files via upload

--- templates/menu.php
+++ templates/menu.php
@@ -3,5 +3,7 @@
 <a href="?pg=inicial">Página inicial</a>  |
 <a href="?pg=form_cadastro">Entre ou Cadastre-se</a>  |
 <a href="admin/admin.php"> Central Admin</a>  |
+<a href="cliente/cortes.php">Cortes</a>  |
+
 
 </nav>
