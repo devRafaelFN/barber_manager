23350707366d4beb95d1d3d8d2bfa6fc62ff06df
devRafaelFN <rafaelpontesfn4@gmail.com>
2024-11-26 21:39:03 -0300

    Add files via upload

---
+++ cliente/formAgenda.php
@@ -0,0 +1,12 @@
+<h3>MARQUE AQUI SEU HORÁRIO</h3>
+
+<form action="?pg=agendamento" method="post">
+    Nome: <input type="text" name="nome" required><br> 
+    Data: <input type="date" name="data"><br>
+    Horário: <input type="time" name="hora" required><br>
+    Observação: <input type="text" name="observacao"><br> 
+    
+    <input type="submit" value="MARCAR"><br> 
+
+
+</form>
